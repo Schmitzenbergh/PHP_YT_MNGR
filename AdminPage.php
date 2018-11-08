eb2e668ddf2723fd22243261853a369f962793d4
Rob Smit <nope@nope.no>
2018-11-08 14:33:20 +0100

    -Basic layout adminpage added -Css added (based on piratenbaai) -Barebone filereading on playbackpage added -Added entry to database.csv

--- AdminPage.php
+++ AdminPage.php
@@ -1,7 +1,19 @@
-<?php
-/**
- * Created by PhpStorm.
- * User: Rob
- * Date: 8-11-2018
- * Time: 13:45
- */
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Youtube Mngr</title>
+    <meta charset="UTF-8">
+    <link href="Style.css" rel="stylesheet" type="text/css">
+</head>
+<body>
+<div id="main">
+    <!--ENCTYPE-->
+    <form action="#" method="post" enctype="multipart/form-data">
+        <label for="file">Filename:</label><input type="file" name="uploadedFile" id="file" />
+
+        <input type="submit" name="submit" value="Aaaargh" />
+    </form>
+    <p><a href="readingDir.php">Laat alles in de baai zien</a></p>
+</div>
+</body>
+</html>
--- PlaybackPage.php
+++ PlaybackPage.php
@@ -1,7 +1,42 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: Rob
- * Date: 8-11-2018
- * Time: 13:43
- */
+    CONST DATABASE_PATH = "database.csv";
+    //Check if get is set
+    if(isset($_GET["id"]))
+    {
+        //Filter potential bad input (EG: not a number)
+        $id = filter_var($_GET["id"], FILTER_SANITIZE_NUMBER_INT);
+
+        if ($id >= 0)
+        {
+            if(file_exists(DATABASE_PATH) && (filesize(DATABASE_PATH) != 0))
+            {
+                $database = explode(";",file(DATABASE_PATH)[1]);
+                $artist = $database[0];
+                $title = $database[1];
+                $pbid = $database[2];
+            }
+        }
+
+    }
+
+
+?>
+
+
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Youtube Mngr - Playback</title>
+    <meta charset="UTF-8">
+    <link href="Style.css" rel="stylesheet" type="text/css">
+</head>
+<body>
+    <div id="main">
+        <?php
+            echo"<h1>". $artist ." - ". $title ."</h1>";
+        ?>
+
+        <iframe width="560" height="315" src="https://www.youtube.com/embed/-tJYN-eG1zk" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+    </div>
+</body>
+</html>
